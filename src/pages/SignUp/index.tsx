70c231477298041336d37e6bb940b3450aa73d1f
rodrigodasilva <ribeiro.rodrigodasilva>
2020-05-26 17:25:16 -0300

    styles: signup page

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { FiMail, FiArrowLeft, FiUser, FiLock } from 'react-icons/fi';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -9,15 +9,17 @@ import logoImg from '../../assets/logo.svg';
 
 import { Container, Content, Background } from './styles';
 
-const SignIn: React.FC = () => {
+const SignUp: React.FC = () => {
   return (
     <Container>
+      <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
         <form>
-          <h1>Faça seu logon</h1>
+          <h1>Faça seu Cadastro</h1>
 
+          <Input name="name" icon={FiUser} placeholder="Nome" />
           <Input name="email" icon={FiMail} placeholder="E-mail" />
 
           <Input
@@ -27,19 +29,16 @@ const SignIn: React.FC = () => {
             placeholder="Senha"
           />
 
-          <Button type="submit">Entrar</Button>
-
-          <a href="forgot">Esqueci minha senha</a>
+          <Button type="submit">Cadastrar</Button>
         </form>
-        <Link to="/signup">
-          <FiLogIn />
-          Criar conta
+
+        <Link to="/">
+          <FiArrowLeft />
+          Voltar para logon
         </Link>
       </Content>
-
-      <Background />
     </Container>
   );
 };
 
-export default SignIn;
+export default SignUp;
