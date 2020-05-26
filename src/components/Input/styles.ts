6a5749ddfbf6943de7623b11bbb494e1a24f77d6
rodrigodasilva <ribeiro.rodrigodasilva>
2020-05-26 17:14:26 -0300

    refactor: create Button and Input components

---
+++ src/components/Input/styles.ts
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #232129;
+  border-radius: 10px;
+  border: 2px solid #232129;
+  padding: 16px;
+  width: 100%;
+  color: #666360;
+  display: flex;
+  align-items: center;
+
+  input {
+    flex: 1;
+    color: #f4ede8;
+    background: transparent;
+    border: 0;
+
+    &::placeholder {
+      color: #666360;
+    }
+  }
+
+  & + div {
+    margin-top: 8px;
+  }
+
+  svg {
+    margin-right: 16px;
+  }
+`;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -14,11 +17,16 @@ const SignIn: React.FC = () => {
         <form>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="E-mail" />
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-          <input type="password" placeholder="Senha" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-          <button type="submit">Entrar</button>
+          <Button type="submit">Entrar</Button>
 
           <a href="forgot">Esqueci minha senha</a>
         </form>
