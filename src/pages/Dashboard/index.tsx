21653d01b4d11f994c4dbb0f4fe21b43ae047bc0
rodrigodasilva <ribeiro.rodrigodasilva>
2020-07-31 21:36:16 -0300

    Perfil do usuario - pagina de perfil

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,6 +6,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
+import { Link } from 'react-router-dom';
 import {
   Container,
   Header,
@@ -144,7 +145,9 @@ const Dashboard: React.FC = () => {
 
             <div>
               <span>Bem-vindo, </span>
-              <strong>{user.name}</strong>
+              <Link to="/profile">
+                <strong>{user.name}</strong>
+              </Link>
             </div>
           </Profile>
 
